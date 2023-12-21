38722b5673bf07e4d4ec81b12b9427a74802f504
Silvar925 <silvarmoonlight@gmail.com>
2023-12-21 16:40:30 +0300

    V1.0.7

---
+++ js/footer.js
@@ -0,0 +1,34 @@
+const header = $("header").empty;
+
+header.append(`
+<div class="upper_footer">
+<ul>
+    <li><a href="#">УЛСУГИ</a></li>
+    <li><a href="#">ОБУЧЕНИЕ</a></li>
+    <li><a href="#">КОНТАКТЫ</a></li>
+</ul>
+
+<div class="phone">
+    <h2>8 000 000 00 00</h2>
+    <p>Телефон записи</p>
+</div>
+
+</div>
+
+<hr>
+
+<div class="bottom_footer">
+<div class="bottom_footer_text">
+    <p class="c">© Центр парикмахерских услуг</p>
+    <p>Политика конфиденциальности</p>
+    <p>Пользовательское соглашение</p>
+</div>
+
+<div class="bottom_footer_icons">
+    <img src="img/whatsap.svg" alt="#">
+    <img src="img/instagram.svg" alt="#">
+    <img src="img/telegram.svg" alt="#">
+</div>
+
+</div>
+`)
---
+++ js/header.js
@@ -0,0 +1,15 @@
+const header = $("header").empty;
+
+header.append(`
+<div class="centerBlock">
+<h1>DIA_SHUGAR</h1>
+<nav>
+    <ul>
+        <li><a href="/">ГЛАВНАЯ</a></li>
+        <li><a href="#service">УСЛУГИ</a></li>
+        <li><a href="#training">ОБУЧЕНИЯ</a></li>
+        <li><a href="#additional-information">КОНТАКТЫ</a></li>
+    </ul>
+</nav>
+</div>
+`)
